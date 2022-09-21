67591313d5564dbae96937e443bc54a672a8c6e7
Alan Mackiewicz <alanmackiewicz@yahoo.com>
2022-09-20 21:23:08 -0400

    hi

---
+++ Dockerize-PHP-Application/app/index.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+<?php
+//$txt = "Hello world!";
+//$x = 5;
+//$y = 10.5;
+//define("TIMEOUT", 300);//how to define a const variable
+//define("HTTP_GET_REQUEST_TYPE" , "getRequest/all");
+//
+////log.info(HTTP_GET_REQUEST_TYPE + "request");
+//
+//print("TIMEOUT " . TIMEOUT);
+//
+//echo $txt;
+//echo '</br>';
+//echo $x;
+//
+//myTest($x);
+//
+//echo "Study PHP at " . $txt . "<br>";//. is used instead of <<
+////var_dump($x);//shows the data type and value
+//
+//function myTest($scopeTest) {
+//
+//    $t = date("H");//date is a built in function
+//
+//    $marks = 40;
+//    print ($marks >= 40) ? "pass" : "Fail";//this code does the same as the if/else below
+//    //best to use if just one else if
+//
+////    if ($marks >= 40) {
+////        echo "pass";
+////    }
+////    else{
+////        echo "fail";
+////    }
+//
+//    $x = 5; // local scope
+//    echo '</br>';
+//    echo "<p>Variable x inside function is: $x</p>";
+//}
+
+//$cars = array("Volvo", "BMW", "Toyota");
+//$arrlength = count($cars);
+//
+//for($i = 0;$i < $arrlength;$i++){
+//    echo $cars[$i];
+//    echo "<br>";
+//}
+//
+//
+//
+//
+//$colors = array("red", "green", "blue" , "yellow");
+//
+//foreach ($colors as $value){//way more concise way to print an array
+//    echo "$value <br> ";
+//}
+//
+//$nameAgeMap = array("Peter" => "35", "Ben" =>"37", "Joe"=>"43");//how a hash array looks
+//
+//$str = "W3SCHOOLS";
+//$pattern = "/w3schools/i";//the i makes it case sensitive
+//echo preg_match($pattern, $str);//prints 1 if pattern appears and 0 if it doesnt
+
+echo '<p>Alans world</p>';
+
+?>
+</body>
+</html>
